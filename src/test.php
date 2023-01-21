10b272be36903a7c3d10c1b75d3b49d5ea7b3b88
Chris Pugmire <chrisp@netwin.co.nz>
2023-01-21 15:02:31 +1300

    my commit

---
+++ src/test.php
@@ -0,0 +1,13 @@
+<?php
+require "myover.php";
+
+$client = myover_open("192.168.1.51",143,"test1@small.com","Mytest2","/notls");
+
+$ov = myoverview($client,"INBOX","100:110");
+
+foreach ($ov as $overview) {
+	echo var_dump($overview),"\n";
+}
+
+
+?>
