3636b6085d0c6948336dc64de5f9cd68660ea07f
Chris Pugmire <chrisp@netwin.co.nz>
2023-01-21 15:24:43 +1300

    my commit

--- src/test.php
+++ src/test.php
@@ -1,7 +1,7 @@
 <?php
 require "backend/imap/myover.php";
 
-define('MSG_MAX_SIZE', 2000000);
+define('MAX_MSG_SIZE',20);
 $client = myover_open("192.168.1.50",143,"test1@small.com","Mytest2","/notls");
 
 $ov = myoverview($client,"INBOX","100:110");
