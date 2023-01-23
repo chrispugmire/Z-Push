5ada9ccb4be1a495afbf939bc4d37874f916230d
Chris Pugmire <chrisp@netwin.co.nz>
2023-01-23 15:48:45 +1300

    my commit

---
+++ install.php
@@ -0,0 +1,39 @@
+<?php
+recurseCopy("src","/usr/share/z-push","");
+echo "\n";
+echo "Now YOU MUST fix ownerships: e.g.     chown -R www-data /usr/share/z-push";
+
+
+function recurseCopy(string $sourceDirectory, string $destinationDirectory): void {
+    $directory = opendir($sourceDirectory);
+    $ncopy = 0;
+    if (is_dir($destinationDirectory) === false) {
+        mkdir($destinationDirectory);
+    }
+    while (($file = readdir($directory)) !== false) {
+        if ($file === '.' || $file === '..') {
+            continue;
+        }
+
+        $fullsrc = $sourceDirectory."/".$file;
+        $fulldst = $destinationDirectory."/".$file;
+        if (is_dir($fullsrc) === true) {
+            recurseCopy($fullsrc,$fulldst);
+        } else {
+            if ($file == "config.php") {
+                if (file_exists($fulldst)) {
+                    echo "Not modifying ".$fulldst."\n";
+                    continue ;
+                }
+            }
+            if (strpos($file,".php")==false) continue;
+            //echo "copy ".$fullsrc." to ".$fulldst."\n";
+            $ncopy += 1;
+            copy($fullsrc,$fulldst);
+        }
+    }
+    echo "Coppied ".$ncopy." files to ".$destinationDirectory."\n";
+
+    closedir($directory);
+    return;
+}
